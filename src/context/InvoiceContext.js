366ab8e1f3c59e62966231a50055515a809a3512
Ajay Srinivas <ajaysrinivas333@gmail.com>
2022-04-16 23:04:43 +0530

    Added default Invoice values in Context and some refactoring

--- src/context/InvoiceContext.js
+++ src/context/InvoiceContext.js
@@ -1,23 +1,15 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from "react";
+import { getInvoiceData } from "./helper";
 
-/*
- TODO  : Create a json file with default values
-
- TODO  : Create a function which gets the default invoices values
-function -> check for localstorage invoices value... 
-if its undefined return default data in json file
-
-*/
-
-// TODO : Set Defaultvalues as provided in json
 const InvoiceContext = createContext({ invoices: [] });
 
 export const InvoiceContextProvider = ({ children }) => {
-	return (
-		<InvoiceContext.Provider value={{ invoices: [] }}>
-			{children}
-		</InvoiceContext.Provider>
-	);
+  const [invoices] = useState(getInvoiceData);
+  return (
+    <InvoiceContext.Provider value={{ invoices: invoices }}>
+      {children}
+    </InvoiceContext.Provider>
+  );
 };
 
 export const useInvoices = () => useContext(InvoiceContext);

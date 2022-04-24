6ca2817c8ea1169460742f33566cec15bf7b1b33
Ajay Srinivas <ajaysrinivas333@gmail.com>
2022-04-24 23:21:15 +0530

    Button Tuning temp-fix

---
+++ src/lib/components/IconButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+const style = {
+  borderRadius: "50%",
+  width: 50,
+  height: 50,
+  outline: "none",
+  border: "none",
+};
+function IconButton(props) {
+  return (
+    <button style={style}>
+      <Image src={props.icon} />
+    </button>
+  );
+}
+
+export default IconButton;

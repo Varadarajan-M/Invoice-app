e0ef157a21cfffd2aa714e21b490dd31386078eb
Ajay Srinivas <ajaysrinivas333@gmail.com>
2022-05-16 23:17:11 +0530

    Form Values Formatting partially completed

--- src/invoice-form/helper.js
+++ src/invoice-form/helper.js
@@ -1,3 +1,23 @@
 /* eslint-disable no-useless-escape */
 export const EMAIL_REGEX =
-	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+export const formatInvoiceData = (formData) => {
+  return {
+    clientName: formData.clientName,
+    clientEmail: formData.clientEmail,
+    createdAt: formData.invoiceDate,
+    senderAddress: {
+      street: formData.fromStreetAddress,
+      city: formData.fromCity,
+      postCode: formData.fromPostcode,
+      country: formData.fromCountry,
+    },
+    clientAddress: {
+      street: formData.toStreetAddress,
+      city: formData.toCity,
+      postCode: formData.toPostcode,
+      country: formData.toCountry,
+    },
+  };
+};

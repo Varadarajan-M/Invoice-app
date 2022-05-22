d5ebef7fc0efd12bf53e8407c9170f3caa1c6da4
Varadarajan-M <varad2k12@gmail.com>
2022-05-23 00:52:09 +0530

    🚀Changed Filter checkbox to radio and completed the invoice filter functionality🚀

---
+++ src/lib/components/Radio.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+
+const RadioInput = (props) => {
+	const { value, label, style, onClick, ...radioProps } = props;
+	return (
+		<FormControlLabel
+			className='w-100'
+			value={value}
+			control={<Radio />}
+			label={label}
+			style={{
+				...style,
+			}}
+			sx={{
+				'.css-ahj2mt-MuiTypography-root': {
+					fontFamily: 'League Spartan, sans-serif',
+					fontWeight: 600,
+				},
+			}}
+			{...radioProps}
+		/>
+	);
+};
+
+export default RadioInput;

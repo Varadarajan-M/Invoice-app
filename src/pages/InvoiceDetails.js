7ab587e2d53185c31c170ffdcba833322907a648
Varadarajan-M <varad2k12@gmail.com>
2022-05-26 23:33:20 +0530

    Added invoice-detail-wrapper layout and small refactor

--- src/pages/InvoiceDetails.js
+++ src/pages/InvoiceDetails.js
@@ -3,14 +3,23 @@ import { useInvoices } from '../context/InvoiceContext';
 import { useParams } from 'react-router-dom';
 
 function InvoiceDetails(props) {
-  const { invoices } = useInvoices();
-  const { id } = useParams();
-  const matching = invoices.filter((invoice) => invoice.id === id)[0];
-  return (
-    <div>
-      <pre>{JSON.stringify(matching, null, 1)}</pre>
-    </div>
-  );
+	const { invoices } = useInvoices();
+	const { id } = useParams();
+	const matching = invoices.filter((invoice) => invoice.id === id)[0];
+  
+	return (
+		<div className='w-100 invoice-detail-wrapper'>
+			<span>Go back </span>
+
+			<header className='invoice-detail-header bg-primary'>
+				Invoice Details header
+			</header>
+
+			<main className='invoice-details-body bg-secondary'>
+				Invoice Detail Body
+			</main>
+		</div>
+	);
 }
 
 export default InvoiceDetails;

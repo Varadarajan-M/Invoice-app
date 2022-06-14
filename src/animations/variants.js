db8ebd7ff33b2c60280548db45b628976c3f6230
Ajay Srinivas <ajaysrinivas333@gmail.com>
2022-06-14 20:29:45 +0530

    Tuned Filter and Transition

--- src/animations/variants.js
+++ src/animations/variants.js
@@ -1,25 +1,25 @@
 export const home = {
-	initial: {
-		opacity: 0,
-		x: 0,
-	},
-	in: {
-		opacity: 1,
-		x: 0,
-		transition: { duration: 1 },
-	},
-	out: {
-		opacity: 0,
-		x: '-100vw',
-		transition: { duration: 1 },
-	},
+  initial: {
+    opacity: 0,
+    x: 0,
+  },
+  in: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 1 },
+  },
+  out: {
+    opacity: 0,
+    x: '-100vw',
+    transition: { duration: 0.5 },
+  },
 };
 
 export const detail = {
-	...home,
-	out: {
-		opacity: 0,
-		x: '100vw',
-		transition: { duration: 1 },
-	},
+  ...home,
+  out: {
+    opacity: 0,
+    x: '100vw',
+    transition: { duration: 0.5 },
+  },
 };
